40b66ffdfc1970516804a5d1b037f9c4b7212c80
Farid Rico <farid.rico@sofka.com.co>
2019-01-25 16:51:03 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap, delay, take, map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ux-ghost';
+export class AppComponent implements OnInit {
+  $posts: Observable<any[]> = null;
+  ghosts = [];
+  loading: boolean;
+
+  constructor(private _http: HttpClient) { }
+
+  ngOnInit() {
+    this.ghosts = Array.from({ length: 4 });
+    this.$posts = this._http.get<any[]>("https://jsonplaceholder.typicode.com/posts")
+      .pipe(
+        tap(() => {
+          this.loading = true
+          this.ghosts = [];
+        })
+      )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
+const MATERIAL_MODULES = [
+  MatSidenavModule,
+  MatToolbarModule,
+  MatCardModule
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +22,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ...MATERIAL_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
